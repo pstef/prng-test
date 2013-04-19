ba922d753cdcdc6f8c8babc4def11eb7111eb0b3
1500100900 <2134918@gmail.com>
2013-04-19 11:30:02 +0200

    Simpler implementation of the Mitchell-Moore algorithm.

--- MM.c
+++ MM.c
@@ -3,33 +3,30 @@
 
 #include "generators.h"
 
-static unsigned long int rgiState[2 + 55];
-static unsigned long int *piState;
+#define L 27U
+#define K 98U
+
+static unsigned long int rgiState[K];
+static unsigned int aState, bState;
 
 void init_mm(unsigned long int seed) {
-  unsigned long int iState;
-  piState = &rgiState[2];
-  piState[-2] = 55 - 55;
-  piState[-1] = 55 - 24;
-  piState[0] = (seed) & ((1 << 30) - 1);
-  piState[1] = 1;
-  for (iState = 2; iState < 55; iState++)
-    piState[iState] =
-        (piState[iState - 1] + piState[iState - 2]) & ((1 << 30) - 1);
+  unsigned int i;
+
+  bState = K - K;
+  aState = K - L;
+
+  for (i = 0; i < K; i++)
+    rgiState[i] = seed = (69069 * seed + 1);
+
   return;
 }
 
 unsigned long int rand_mm(void) {
-  unsigned long int iState1, iState2, iRand;
-  iState1 = piState[-2];
-  iState2 = piState[-1];
-  iRand = (piState[iState1] + piState[iState2]) & ((1 << 30) - 1);
-  piState[iState1] = iRand;
-  if (++iState1 == 55)
-    iState1 = 0;
-  if (++iState2 == 55)
-    iState2 = 0;
-  piState[-2] = iState1;
-  piState[-1] = iState2;
-  return iRand >> 6;
+  if (++bState == K)
+    bState = 0;
+
+  if (++aState == K)
+    aState = 0;
+
+  return (rgiState[bState] += rgiState[aState]) >> 6;
 }
