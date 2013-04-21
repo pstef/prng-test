b3857db800107604e1f9f8de09eb9d669a74d3ff
1500100900 <2134918@gmail.com>
2013-04-21 11:34:40 +0200

    Improve speed and quality of randomness.
    - include another term in the recurrence
    - replace two branches in the generator with modulo (expressed as bitwise AND, because the modulus is a power of 2)
    - to meet the new method's requirement, make sure that the initial values are not all odd and not all even

--- MMopt.c
+++ MMopt.c
@@ -1,41 +1,34 @@
-/* Based on Mitchell-Moore algorithm from
- * The Art of Computer Programming, Volume II, by Donald Knuth */
+/* Mitchell-Moore algorithm from
+ * The Art of Computer Programming, Volume II, by Donald Knuth
+ * 
+ * Improvements based on "Uniform Random Number Generators for Vector
+ * and Parallel Computers" by Richard P. Brent */
 
 #include "generators.h"
 
-static unsigned long int rgi[32] = {
-  0x00000011, 0x9a319039, 0x32d9c024, 0x9b663182, 0x5da1f342, 0xde3b81e0,
-  0xdf0a6fb5, 0xf103bc02, 0x48f340fb, 0x7449e56b, 0xbeb1dbb0, 0xab5c5918,
-  0x946554fd, 0x8c2e680f, 0xeb3d799f, 0xb11ee0b7, 0x2d436b86, 0xda672e2a,
-  0x1588ca88, 0xe369735d, 0x904f35f7, 0xd7158fd6, 0x6fa6f051, 0x616e6b96,
-  0xac94efdc, 0x36413f93, 0xc622c298, 0xf5a42ab8, 0x8a88d77b, 0xf5ad9d0e,
-  0x8999220b, 0x27fb47b9
-};
+#define R 128U
+#define S 82U
+#define T 53U
 
-static unsigned long int *fp = &rgi[4];
-static unsigned long int *rp = &rgi[1];
-static unsigned long int *piState = &rgi[1];
-
-unsigned long int rand_mmopt(void) {
-  unsigned long int i;
-  *fp += *rp;
-  i = (*fp >> 1) & 0x7fffffff;
-  if (++fp >= &rgi[32]) {
-    fp = piState;
-    ++rp;
-  } else if (++rp >= &rgi[32])
-    rp = piState;
-  return i >> 6;
-}
+static unsigned long int sequence[R];
+static unsigned int a, b, c;
 
 void init_mmopt(unsigned long int seed) {
-  int i = 1;
-  piState[0] = seed;
-  for (i = 1; i < 31; i++)
-    piState[i] = 1103515245 * piState[i - 1] + 12345;
-  fp = &piState[3];
-  rp = &piState[0];
-  for (i = 0; i < 10 * 31; i++)
-    rand_mmopt();
+  unsigned int i;
+
+  a = R - R;
+  b = R - S;
+  c = R - T;
+
+  for (i = 0; i < R * 2; i++)
+    sequence[i % R] = seed = (1664525 * seed + 1013904223);
+
+  sequence[0] <<= 1;
+  sequence[1] |= 1;
+
   return;
+}
+
+unsigned long int rand_mmopt(void) {
+  return sequence[++a & 127] += sequence[++b & 127] + sequence[++c & 127];
 }
