a3f23cc3dbec368c26d1b45b51325cbab3443260
1500100900 <2134918@gmail.com>
2013-04-21 16:49:41 +0200

    Resolve missing prototypes issue.

--- MM.c
+++ MM.c
@@ -3,17 +3,21 @@
 
 #include "generators.h"
 
+static inline unsigned long int rand_mm1(void);
+static inline unsigned long int rand_mm2(void);
+static inline unsigned long int rand_mm3(void);
+
 #define L 37U
 #define K 100U
 
 static unsigned long int sequence[K];
-static unsigned int a, b;
+static unsigned int b, a;
 
 void init_mm(unsigned long int seed) {
   unsigned int i;
 
-  b = K;
-  a = L;
+  a = K;
+  b = L;
 
   for (i = 0; i < K * 2; i++)
     sequence[i % K] = seed = (1664525 * seed + 1013904223);
@@ -21,27 +25,32 @@ void init_mm(unsigned long int seed) {
   return;
 }
 
+/* This way I export one of the implementations below */
+unsigned long int rand_mm(void) {
+  return rand_mm3();
+}
+
+
+/* Implementations of the algorithm: */
 
 /* Clear version */
-unsigned long int rand_mm1(void) {
-  if (b == K)
-    b = 0;
-  else if (a == K)
+static inline unsigned long int rand_mm1(void) {
+  if (a == K)
     a = 0;
+  else if (b == K)
+    b = 0;
 
-  return sequence[b++] += sequence[a++];
+  return sequence[a++] += sequence[b++];
 }
-
 
 /* Alternative clear version */
-unsigned long int rand_mm2(void) {
+static inline unsigned long int rand_mm2(void) {
   a++;
   b++;
-  return sequence[b % K] += sequence[a % K];
+  return sequence[a % K] += sequence[b % K];
 }
 
-
 /* Faster version */
-unsigned long int rand_mm(void) {
-  return sequence[b = (b == K) ? 0 : b + 1] += sequence[a = (a == K) ? 0 : a + 1];
+static inline unsigned long int rand_mm3(void) {
+  return sequence[a = (a == K) ? 0 : a + 1] += sequence[b = (b == K) ? 0 : b + 1];
 }
