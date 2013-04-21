e8a5aa7a6e3d9d0797b855022ce96aaf38ae9fa0
1500100900 <2134918@gmail.com>
2013-04-21 15:02:56 +0200

    Provide two clear versions, drop preprocessor directives, fix broken code.
    - I moved an alternative version from a comment to an implementation, so it's easier to read with highlighting
    - I didn't know that github turns off highlighting for code included within an #if preprocessor directive
    - the first clear alternative was broken.

--- MM.c
+++ MM.c
@@ -21,23 +21,27 @@ void init_mm(unsigned long int seed) {
   return;
 }
 
-#if 0
-/* Clearer version. Alternative clear version would just:
-   return sequence[b % K] += sequence[a % K]; */
-unsigned long int rand_mm(void) {
-  if (++b == K)
-    b = 0;
 
-  if (++a == K)
+/* Clear version */
+unsigned long int rand_mm1(void) {
+  if (b == K)
+    b = 0;
+  else if (a == K)
     a = 0;
 
-  return sequence[b] += sequence[a];
+  return sequence[b++] += sequence[a++];
 }
 
-#else
+
+/* Alternative clear version */
+unsigned long int rand_mm2(void) {
+  a++;
+  b++;
+  return sequence[b % K] += sequence[a % K];
+}
+
+
 /* Faster version */
 unsigned long int rand_mm(void) {
   return sequence[b = (b == K) ? 0 : b + 1] += sequence[a = (a == K) ? 0 : a + 1];
 }
-
-#endif
